09b7d67769317d70a922d7575d9762a1ec347d50
Pablo Tobalina <pablotobalina@gmail.com>
2023-12-03 12:28:42 +0100

    create message component

--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MessageComponent } from "../message/message.component";
 
 @Component({
-  selector: 'app-topic',
-  standalone: true,
-  imports: [],
-  templateUrl: './topic.component.html',
-  styleUrl: './topic.component.scss'
+    selector: 'app-topic',
+    standalone: true,
+    templateUrl: './topic.component.html',
+    styleUrl: './topic.component.scss',
+    imports: [MessageComponent]
 })
 export class TopicComponent {
 
