2ec0ed113cb0de42afd4a27f24bd6d1f18553da2
Pablo Tobalina <pablotobalina@gmail.com>
2023-12-03 12:05:02 +0100

    empty producer - topic - consumer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ConsumerComponent } from "./consumer/consumer.component";
+import { ProducerComponent } from "./producer/producer.component";
+import { TopicComponent } from "./topic/topic.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, ConsumerComponent, ProducerComponent, TopicComponent]
 })
 export class AppComponent {
   title = 'kafka-playground';
