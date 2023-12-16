7c4217ec8eab7f5ef402180c700b00351a0a423a
Pablo Tobalina <pablotobalina@gmail.com>
2023-12-16 12:18:06 +0100

    partitions with angular material

--- src/app/topic/partition/partition.component.ts
+++ src/app/topic/partition/partition.component.ts
@@ -2,13 +2,14 @@ import { Component, Input, Signal } from '@angular/core';
 import { Message } from '../../message/message';
 import { MessageComponent } from "../../message/message.component";
 import { Partition } from './partition';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
     selector: 'app-partition',
     standalone: true,
     templateUrl: './partition.component.html',
     styleUrl: './partition.component.scss',
-    imports: [MessageComponent]
+    imports: [MessageComponent, MatCardModule]
 })
 export class PartitionComponent {
   @Input() partition!: Partition;
