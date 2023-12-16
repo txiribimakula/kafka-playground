b9a0ae0d1cd635d32ccaf2c249c6cf39aa8f59b0
Pablo Tobalina <pablotobalina@gmail.com>
2023-12-16 11:29:01 +0100

    producer with angular material

--- src/app/producer/producer.component.ts
+++ src/app/producer/producer.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { KafkaService } from '../kafka.service';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-producer',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatCardModule, MatSelectModule, MatButtonModule],
   templateUrl: './producer.component.html',
   styleUrl: './producer.component.scss',
 })
 export class ProducerComponent {
-  topic: string = "one.topic";
+  topic: string = 'one.topic';
 
   constructor(private kafka: KafkaService) {}
 
