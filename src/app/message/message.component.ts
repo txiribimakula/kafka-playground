12dfae0afe9bfeaa1d6f1bc94d5ce2007fb431a7
Pablo Tobalina <pablotobalina@gmail.com>
2023-12-16 12:16:27 +0100

    messages with angulr material

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Message } from './message';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-message',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss',
 })
