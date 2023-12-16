fa34d95fdd49bf113295335e70482f0d64793ec6
Pablo Tobalina <pablotobalina@gmail.com>
2023-12-16 12:23:14 +0100

    topics with angular material

--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -4,13 +4,14 @@ import { PartitionComponent } from './partition/partition.component';
 import { Partition } from './partition/partition';
 import { TopicService } from './topic.service';
 import { ConsumerGroupService } from '../consumer/consumer-group/consumer-group.service';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-topic',
   standalone: true,
   templateUrl: './topic.component.html',
   styleUrl: './topic.component.scss',
-  imports: [MessageComponent, PartitionComponent],
+  imports: [MessageComponent, PartitionComponent, MatCardModule],
 })
 export class TopicComponent implements OnInit {
   @Input({ required: true }) name!: string;
