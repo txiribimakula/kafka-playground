b93f43003d65d9032da7956fcd1029ced855bdaa
Pablo Tobalina <pablotobalina@gmail.com>
2023-12-16 12:04:52 +0100

    format producer card

--- src/app/producer/producer.component.ts
+++ src/app/producer/producer.component.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-producer',
   standalone: true,
-  imports: [FormsModule, MatCardModule, MatSelectModule, MatButtonModule],
+  imports: [FormsModule, MatCardModule, MatSelectModule, MatButtonModule, MatDividerModule],
   templateUrl: './producer.component.html',
   styleUrl: './producer.component.scss',
 })
