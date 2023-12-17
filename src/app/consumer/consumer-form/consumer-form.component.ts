428d45a986942224fced8730355d172c4452fb31
Pablo Tobalina <pablotobalina@gmail.com>
2023-12-17 14:53:39 +0100

    select existing topics to create consumer

--- src/app/consumer/consumer-form/consumer-form.component.ts
+++ src/app/consumer/consumer-form/consumer-form.component.ts
@@ -4,11 +4,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { ConsumerService } from '../consumer.service';
+import { TopicService } from '../../topic/topic.service';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-consumer-form',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, FormsModule, MatSelectModule],
   templateUrl: './consumer-form.component.html',
   styleUrl: './consumer-form.component.scss',
 })
@@ -16,7 +18,7 @@ export class ConsumerFormComponent {
   topicName: string = '';
   groupId: string = '';
 
-  constructor(private consumer: ConsumerService) {}
+  constructor(private consumer: ConsumerService, protected topic: TopicService) {}
 
   addConsumer() {
     this.consumer.addConsumer([this.topicName], this.groupId);
