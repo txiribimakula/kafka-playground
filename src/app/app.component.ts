77bd7233913844b6b91a34dbe4b3c36858bbb13a
Pablo Tobalina <pablotobalina@gmail.com>
2023-12-17 11:57:24 +0100

    move app to kafka component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ConsumerComponent } from "./consumer/consumer.component";
-import { ProducerComponent } from "./producer/producer.component";
-import { TopicComponent } from "./topic/topic.component";
-import { ConsumerGroupComponent } from "./consumer/consumer-group/consumer-group.component";
-import { ConsumerGroupService } from './consumer/consumer-group/consumer-group.service';
-import { TopicService } from './topic/topic.service';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, ConsumerComponent, ProducerComponent, TopicComponent, ConsumerGroupComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [CommonModule, RouterOutlet],
 })
-export class AppComponent {
-  constructor(protected topic: TopicService, protected consumerGroup: ConsumerGroupService) {
-  }
-}
+export class AppComponent {}
--- src/app/kafka/kafka.component.ts
+++ src/app/kafka/kafka.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { ProducerComponent } from '../producer/producer.component';
+import { TopicComponent } from '../topic/topic.component';
+import { ConsumerGroupComponent } from '../consumer/consumer-group/consumer-group.component';
+import { TopicService } from '../topic/topic.service';
+import { ConsumerGroupService } from '../consumer/consumer-group/consumer-group.service';
 
 @Component({
   selector: 'app-kafka',
   standalone: true,
-  imports: [],
   templateUrl: './kafka.component.html',
-  styleUrl: './kafka.component.scss'
+  styleUrl: './kafka.component.scss',
+  imports: [ProducerComponent, TopicComponent, ConsumerGroupComponent],
 })
 export class KafkaComponent {
-
+  constructor(
+    protected topic: TopicService,
+    protected consumerGroup: ConsumerGroupService
+  ) {}
 }
